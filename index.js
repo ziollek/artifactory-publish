76b6e0c3995793df455ffb9b15c46359a5fdaa47
Remigiusz Herba <remigiusz.herba@allegro.pl>
2021-07-12 09:55:37 +0200

    revert everything.

--- index.js
+++ index.js
@@ -15,11 +15,10 @@ const tychoPath = core.getInput('tycho');
 const targetPath = group.replace(/\./g, '/');
 const isSnapshot = version.endsWith('-SNAPSHOT');
 
-const deployPackage = name + "-" + version + '-deploy.zip';// : version + '-deploy.zip';
+const deployPackage = version + '-deploy.zip';
 exec(`zip --quiet --symlinks --recurse-paths "${deployPackage}" ${buildDir} --exclude "${deployPackage}"`, (error) => {
     if (error) {
-        // console.error(`package ${deployPackage}`);
-        console.error(`exec error: ${error} package: ${deployPackage}`);
+        console.error(`exec error: ${error}`);
         process.exit(1);
     }
 
