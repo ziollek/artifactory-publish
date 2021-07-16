2c693021c85cd9a1ca605af02a4bfa2cb2972b91
Bartosz Galek <bartosz.galek@allegro.pl>
2021-07-16 15:54:47 +0200

    docker testing

--- packaging/docker.js
+++ packaging/docker.js
@@ -1,11 +1,19 @@
-const fs = require('fs');
 const { execSync: exec } = require('child_process');
 const core = require('@actions/core');
 
 module.exports = function (host, username, password, name, group, version) {
   exec(`curl -u ${username}:${password} -s https://${host}/v2/auth > .dockercfg`);
-  const fileContents = String(fs.readFileSync('.dockercfg', 'utf8'));
-  fs.writeFileSync('.dockercfg', fileContents.replace(/https:\/\/localhost:8081/gm, `https://${host}`));
-  const stdout = exec(`docker --config .dockercfg push ${host}/${group}/${name}:${version}`);
+  const command = `
+    IMAGEID=$(uuidgen)
+    IMAGETAG="${host}/${group}/${name}:${version}"
+    docker build -t $IMAGETAG:$IMAGEID .
+    docker tag $IMAGETAG:$IMAGEID $IMAGETAG:${version}
+    echo '{"auths": ' >> config.json
+    curl -u ${username}:${password} -s "${host}/v2/auth" >> config.json
+    echo " }" >> config.json
+    sed -i 's%localhost:8081%${host}%' config.json
+    docker --config=./ push $IMAGETAG:${version}
+  `.replace(/\n/g, '\\\n');
+  const stdout = exec(command);
   core.info(String(stdout).trim());
 };
