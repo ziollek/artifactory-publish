8cd8b159b3a85cf2b409742f71c769407701be91
Bartosz Galek <bartosz.galek@allegro.pl>
2021-08-02 17:49:42 +0200

    added docker provisioning file #8

--- docker/index.js
+++ docker/index.js
@@ -1,6 +1,10 @@
 const { execSync: exec } = require('child_process');
 const core = require('@actions/core');
 const uuid = require('uuid');
+const fs = require('fs');
+const { provisioningArtifactUrl } = require('../utils/artifactory');
+const { compressFiles } = require('../utils/compress');
+const fetch = require('node-fetch');
 
 const host = core.getInput('host');
 const username = core.getInput('username');
@@ -10,9 +14,15 @@ const path = core.getInput('path');
 const version = core.getInput('version');
 const dockerfile = core.getInput('dockerfile') || 'Dockerfile';
 const context = core.getInput('context') || '.';
+const tychoPath = core.getInput('tycho');
 
 const temp = uuid.v4();
 const imageTag = `${host}/${path}/${name}`;
+
+const currentBranch = process.env['GITHUB_HEAD_REF'] || process.env['GITHUB_REF'].split('/').pop();
+core.info(`current branch: ${currentBranch}`);
+const isSnapshot = !['master', 'main'].includes(currentBranch);
+if (isSnapshot) core.info('this is a snapshot release');
 
 try {
   exec(`docker login -u ${username} -p ${password} ${host}`);
@@ -25,4 +35,27 @@ try {
   core.info(`docker push finished ${imageTag}:${temp} ${imageTag}:${version}`);
 } catch (e) {
   core.setFailed(e);
+}
+
+if (fs.existsSync(tychoPath)) {
+  fs.writeFileSync('dependencies.yml', 'datasources:\nservices:\n');
+  fs.writeFileSync('environment-variables.yml', 'envs:\n');
+  fs.writeFileSync('deployment.yml', fs.readFileSync(tychoPath, 'utf8'));
+
+  const target = provisioningArtifactUrl(username, password, host, path, name, version, currentBranch, isSnapshot);
+  compressFiles(['deployment.yml', 'environment-variables.yml', 'dependencies.yml'])
+    .then(data => fetch(target.toString(), { method: 'PUT', body: data }).then(response => response.status))
+    .then((status) => {
+      core.info(`[provisioning package] artifactory response: ${status}`);
+      if (status >= 300) {
+        throw new Error('provisioning package upload failed');
+      }
+    })
+    .then(() => {
+      target.username = null;
+      target.password = null;
+      core.info(`${target} uploaded.`);
+      core.setOutput('url', target.toString());
+    })
+    .catch(reason => core.setFailed(reason));
 }
--- maven/index.js
+++ maven/index.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const fetch = require('node-fetch');
 const core = require('@actions/core');
-const { compressDirectory, compressFiles } = require('./compress');
-const { deployArtifactUrl, provisioningArtifactUrl } = require('./artifactory');
+const { compressDirectory, compressFiles } = require('../utils/compress');
+const { deployArtifactUrl, provisioningArtifactUrl } = require('../utils/artifactory');
 
 const host = core.getInput('host');
 const username = core.getInput('username');
@@ -40,7 +40,9 @@ function publishDistributions() {
     })
     .then((status) => {
       core.info(`[deploy package] artifactory response: ${status}`);
-      checkStatus(status);
+      if (status >= 300) {
+        throw new Error('provisioning package upload failed');
+      }
     })
     .then(() => {
       const url = deployArtifactUrl('', '', host, group, name, version, currentBranch, isSnapshot);
@@ -57,7 +59,9 @@ function publishBuildDir() {
     .then(data => fetch(target.toString(), { method: 'PUT', body: data }).then(response => response.status))
     .then((status) => {
       core.info(`[deploy package] artifactory response: ${status}`);
-      checkStatus(status);
+      if (status >= 300) {
+        throw new Error('provisioning package upload failed');
+      }
     })
     .then(() => {
       const url = deployArtifactUrl('', '', host, group, name, version, currentBranch, isSnapshot);
@@ -73,25 +77,20 @@ if (fs.existsSync(tychoPath)) {
   fs.writeFileSync('dependencies.yml', 'datasources:\nservices:\n');
   fs.writeFileSync('environment-variables.yml', 'envs:\n');
   fs.writeFileSync('deployment.yml', fs.readFileSync(tychoPath, 'utf8'));
-  const target = provisioningArtifactUrl(username, password, host, group, name, version, currentBranch, isSnapshot).toString();
+  const target = provisioningArtifactUrl(username, password, host, group, name, version, currentBranch, isSnapshot);
   compressFiles(['deployment.yml', 'environment-variables.yml', 'dependencies.yml'])
-    .then(data => fetch(target, { method: 'PUT', body: data }).then(response => response.status))
+    .then(data => fetch(target.toString(), { method: 'PUT', body: data }).then(response => response.status))
     .then((status) => {
       core.info(`[provisioning package] artifactory response: ${status}`);
-      checkStatus(status);
+      if (status >= 300) {
+        throw new Error('provisioning package upload failed');
+      }
     })
     .then(() => {
-      const url = provisioningArtifactUrl('', '', host, group, name, version, currentBranch, isSnapshot);
-      url.username = null;
-      url.password = null;
-      core.info(`${provisioningArtifactUrl} uploaded.`);
-      core.setOutput('url', url);
+      target.username = null;
+      target.password = null;
+      core.info(`${target} uploaded.`);
+      core.setOutput('url', target);
     })
     .catch(reason => core.setFailed(reason));
-}
-
-function checkStatus(status) {
-  if (status >= 300) {
-    throw new Error('provisioning package upload failed');
-  }
 }
