52ac650d77f60b79aee05ab2a5188228413f1007
Bartosz Galek <bartosz.galek@allegro.pl>
2021-08-20 00:15:42 +0200

    dist cleanup, removed -1 suffix

--- utils/artifactory.js
+++ utils/artifactory.js
@@ -1,28 +1,32 @@
 module.exports = {
-  deployArtifactUrl(username, password, host, group, name, version, currentBranch, isSnapshot, timestampFunction = getTimestamp) {
-    return artifactUrl(username, password, host, group, name, version, currentBranch, isSnapshot, timestampFunction, '-deploy.zip');
+  deployArtifactUrl(username, password, host, group, name, version, currentBranch, isSnapshot) {
+    return artifactUrl(username, password, host, group, name, version, currentBranch, isSnapshot, '-deploy.zip');
   },
 
-  provisioningArtifactUrl(username, password, host, group, name, version, currentBranch, isSnapshot, timestampFunction = getTimestamp) {
-    return artifactUrl(username, password, host, group, name, version, currentBranch, isSnapshot, timestampFunction, '-provisioning.zip');
+  provisioningArtifactUrl(username, password, host, group, name, version, currentBranch, isSnapshot) {
+    return artifactUrl(username, password, host, group, name, version, currentBranch, isSnapshot, '-provisioning.zip');
   }
 };
 
-function getTimestamp() {
-  return new Date().toISOString().replace(/\..+/, '').replace('T', '.').replace(/:/g, '').replace(/-/g, '') + '-1';
-}
-
-function artifactUrl(username, password, host, group, name, version, currentBranch, isSnapshot, timestampFunction, fileNameSuffix = '.zip') {
+function artifactUrl(username, password, host, group, name, version, currentBranch, isSnapshot, fileNameSuffix = '.zip') {
   const snapshotSuffix = isSnapshot ? '-SNAPSHOT' : '';
-  const snapshotFilenameSuffix = isSnapshot ? `-${timestampFunction()}` : '';
-  const brachSuffix = isSnapshot ? `-${slugify(currentBranch)}` : '';
+  const snapshotFilenameSuffix = isSnapshot ? `-${getTimestamp()}` : '';
+  const branchSuffix = isSnapshot ? `-${slugify(currentBranch)}` : '';
   const targetPath = group.replace(/\./g, '/');
-  const targetVersion = `${version}${brachSuffix}`;
+  const targetVersion = `${version}${branchSuffix}`;
   const targetFileName = `${name}-${targetVersion}${snapshotFilenameSuffix}${fileNameSuffix}`;
   return new URL(
     `/artifactory/allegro-${isSnapshot ? 'snapshots' : 'releases'}-local/${targetPath}/${name}/${targetVersion}${snapshotSuffix}/${targetFileName}`,
     `https://${encodeURIComponent(username)}:${encodeURIComponent(password)}@${host}`
   );
+}
+
+function getTimestamp() {
+  return new Date().toISOString()
+    .replace(/\..+/, '')
+    .replace('T', '.')
+    .replace(/:/g, '')
+    .replace(/-/g, '');
 }
 
 function slugify(input) {
--- utils/artifactory.spec.js
+++ utils/artifactory.spec.js
@@ -1,9 +1,11 @@
 const { deployArtifactUrl, provisioningArtifactUrl } = require('./artifactory');
 
 describe('artifactory', () => {
+
   beforeEach(() => {
     jest.spyOn(Date.prototype, 'toISOString').mockReturnValue('2021-08-19T11:26:06.000Z');
   });
+
   const TEST_CASES = [
     {
       name: 'opbox-web',
@@ -18,8 +20,8 @@ describe('artifactory', () => {
       version: '1.0.0',
       branch: 'my-branch',
       snapshot: true,
-      deployUrl: 'https://bgalek:password@company.artifactory/artifactory/allegro-snapshots-local/pl/allegro/opbox/opbox-web/1.0.0-my-branch-SNAPSHOT/opbox-web-1.0.0-my-branch-20210819.112606-1-deploy.zip',
-      provisioningUrl: 'https://bgalek:password!%40%23@company.artifactory/artifactory/allegro-snapshots-local/pl/allegro/opbox/opbox-web/1.0.0-my-branch-SNAPSHOT/opbox-web-1.0.0-my-branch-20210819.112606-1-provisioning.zip'
+      deployUrl: 'https://bgalek:password@company.artifactory/artifactory/allegro-snapshots-local/pl/allegro/opbox/opbox-web/1.0.0-my-branch-SNAPSHOT/opbox-web-1.0.0-my-branch-20210819.112606-deploy.zip',
+      provisioningUrl: 'https://bgalek:password!%40%23@company.artifactory/artifactory/allegro-snapshots-local/pl/allegro/opbox/opbox-web/1.0.0-my-branch-SNAPSHOT/opbox-web-1.0.0-my-branch-20210819.112606-provisioning.zip'
     },
     {
       name: 'listing-mobile-bff',
@@ -34,16 +36,16 @@ describe('artifactory', () => {
       version: '1.0.0',
       branch: 'feature/my-branch',
       snapshot: true,
-      deployUrl: 'https://bgalek:password@company.artifactory/artifactory/allegro-snapshots-local/pl/allegro/opbox/listing-mobile-bff/1.0.0-feature-my-branch-SNAPSHOT/listing-mobile-bff-1.0.0-feature-my-branch-20210819.112606-1-deploy.zip',
-      provisioningUrl: 'https://bgalek:password!%40%23@company.artifactory/artifactory/allegro-snapshots-local/pl/allegro/opbox/listing-mobile-bff/1.0.0-feature-my-branch-SNAPSHOT/listing-mobile-bff-1.0.0-feature-my-branch-20210819.112606-1-provisioning.zip'
+      deployUrl: 'https://bgalek:password@company.artifactory/artifactory/allegro-snapshots-local/pl/allegro/opbox/listing-mobile-bff/1.0.0-feature-my-branch-SNAPSHOT/listing-mobile-bff-1.0.0-feature-my-branch-20210819.112606-deploy.zip',
+      provisioningUrl: 'https://bgalek:password!%40%23@company.artifactory/artifactory/allegro-snapshots-local/pl/allegro/opbox/listing-mobile-bff/1.0.0-feature-my-branch-SNAPSHOT/listing-mobile-bff-1.0.0-feature-my-branch-20210819.112606-provisioning.zip'
     },
     {
       name: 'opbpox-core',
       version: '1',
       branch: 'feature/my-branch',
       snapshot: true,
-      deployUrl: 'https://bgalek:password@company.artifactory/artifactory/allegro-snapshots-local/pl/allegro/opbox/opbpox-core/1-feature-my-branch-SNAPSHOT/opbpox-core-1-feature-my-branch-20210819.112606-1-deploy.zip',
-      provisioningUrl: 'https://bgalek:password!%40%23@company.artifactory/artifactory/allegro-snapshots-local/pl/allegro/opbox/opbpox-core/1-feature-my-branch-SNAPSHOT/opbpox-core-1-feature-my-branch-20210819.112606-1-provisioning.zip'
+      deployUrl: 'https://bgalek:password@company.artifactory/artifactory/allegro-snapshots-local/pl/allegro/opbox/opbpox-core/1-feature-my-branch-SNAPSHOT/opbpox-core-1-feature-my-branch-20210819.112606-deploy.zip',
+      provisioningUrl: 'https://bgalek:password!%40%23@company.artifactory/artifactory/allegro-snapshots-local/pl/allegro/opbox/opbpox-core/1-feature-my-branch-SNAPSHOT/opbpox-core-1-feature-my-branch-20210819.112606-provisioning.zip'
     },
     {
       name: 'opbpox-core',
