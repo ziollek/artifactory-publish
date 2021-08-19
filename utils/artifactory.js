c9b277498889c4dcd50bf9a241921f754a7b67bf
Arkadiusz Adamski <arkadiusz.adamski@allegro.pl>
2021-08-19 13:36:37 +0200

    Add timestamp to snapshots artifacts

--- utils/artifactory.js
+++ utils/artifactory.js
@@ -1,21 +1,26 @@
 module.exports = {
-  deployArtifactUrl(username, password, host, group, name, version, currentBranch, isSnapshot) {
-    return artifactUrl(username, password, host, group, name, version, currentBranch, isSnapshot, '-deploy.zip');
+  deployArtifactUrl(username, password, host, group, name, version, currentBranch, isSnapshot, timestampFunction = getTimestamp) {
+    return artifactUrl(username, password, host, group, name, version, currentBranch, isSnapshot, timestampFunction, '-deploy.zip');
   },
 
-  provisioningArtifactUrl(username, password, host, group, name, version, currentBranch, isSnapshot) {
-    return artifactUrl(username, password, host, group, name, version, currentBranch, isSnapshot, '-provisioning.zip');
+  provisioningArtifactUrl(username, password, host, group, name, version, currentBranch, isSnapshot, timestampFunction = getTimestamp) {
+    return artifactUrl(username, password, host, group, name, version, currentBranch, isSnapshot, timestampFunction, '-provisioning.zip');
   }
 };
 
-function artifactUrl(username, password, host, group, name, version, currentBranch, isSnapshot, fileNameSuffix = '.zip') {
+function getTimestamp() {
+  return new Date().toISOString().replace(/\..+/, '').replace('T', '.').replace(/:/g, '').replace(/-/g, '') + '-1';
+}
+
+function artifactUrl(username, password, host, group, name, version, currentBranch, isSnapshot, timestampFunction, fileNameSuffix = '.zip') {
   const snapshotSuffix = isSnapshot ? '-SNAPSHOT' : '';
+  const snapshotFilenameSuffix = isSnapshot ? `-${timestampFunction()}` : '';
   const brachSuffix = isSnapshot ? `-${slugify(currentBranch)}` : '';
   const targetPath = group.replace(/\./g, '/');
-  const targetVersion = `${version}${brachSuffix}${snapshotSuffix}`;
-  const targetFileName = `${name}-${targetVersion}${fileNameSuffix}`;
+  const targetVersion = `${version}${brachSuffix}`;
+  const targetFileName = `${name}-${targetVersion}${snapshotFilenameSuffix}${fileNameSuffix}`;
   return new URL(
-    `/artifactory/allegro-${isSnapshot ? 'snapshots' : 'releases'}-local/${targetPath}/${name}/${targetVersion}/${targetFileName}`,
+    `/artifactory/allegro-${isSnapshot ? 'snapshots' : 'releases'}-local/${targetPath}/${name}/${targetVersion}${snapshotSuffix}/${targetFileName}`,
     `https://${encodeURIComponent(username)}:${encodeURIComponent(password)}@${host}`
   );
 }
