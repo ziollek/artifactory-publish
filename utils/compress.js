eef2ba0eb828ef0a650a72145dd453f7c16fcdb2
Bartosz Galek <bartosz.galek@allegro.pl>
2021-08-31 15:13:42 +0200

    node12 api used

--- utils/compress.js
+++ utils/compress.js
@@ -9,7 +9,7 @@ module.exports = {
     const { error } = await exec(`cd ${source} && zip --quiet --symlinks --recurse-paths "$OLDPWD/${target}" * --exclude "$OLDPWD/${target} && cd $OLDPWD"`);
     if (error) throw new Error(`zip command error: ${error}`);
     const buffer = fs.readFileSync(target);
-    fs.rmdirSync(target);
+    fs.unlinkSync(target);
     return buffer;
   }
 };
