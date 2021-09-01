ca00e9d6af64159228bbcb820f290e8d9fbc1faf
Bartosz Gałek <bartosz.galek@allegro.pl>
2021-09-01 17:42:20 +0200

    Update git-commands.spec.js

--- utils/tests/git-commands.spec.js
+++ utils/tests/git-commands.spec.js
@@ -2,13 +2,13 @@ const {getBranchName} = require('../git-commands');
 
 describe('git commands', () => {
   beforeEach(() => {
-    delete process.env['GITHUB_BASE_REF'];
+    delete process.env['GITHUB_HEAD_REF'];
     delete process.env['GITHUB_REF'];
   });
 
   test('should return git branch name when there is PR open for that branch', () => {
     // setup
-    process.env['GITHUB_BASE_REF'] = 'feature/TICKET-1234-my-branch';
+    process.env['GITHUB_HEAD_REF'] = 'feature/TICKET-1234-my-branch';
     process.env['GITHUB_REF'] = 'refs/pull/123/merge';
 
     // expect
